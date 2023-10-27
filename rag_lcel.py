6a8cd7b3f566f09c22f3ef2dc7ff4b8ffd975ab3
michelderu <michel.deru@datastax.com>
2023-10-27 21:15:57 +0200

    Better flow

--- rag_lcel.py
+++ rag_lcel.py
@@ -1,5 +1,5 @@
 import tempfile
-import logging
+from logging import getLogger
 import os
 import pandas as pd
 from operator import itemgetter
@@ -24,6 +24,9 @@ from langchain.callbacks.base import BaseCallbackHandler
 from langchain.schema.runnable import RunnableMap
 
 from langchain.document_loaders import PyPDFLoader
+
+log = getLogger()
+log.info('Starting app')
 
 # Streaming call back handler for responses
 class StreamHandler(BaseCallbackHandler):
@@ -89,7 +92,7 @@ def vectorize_text(uploaded_file):
             docs = []
             temp_dir = tempfile.TemporaryDirectory()
             file = uploaded_file
-            logging.info("""Processing: {file}""")
+            log.info("""Processing: {file}""")
             temp_filepath = os.path.join(temp_dir.name, file.name)
             with open(temp_filepath, 'wb') as f:
                 f.write(file.getvalue())
@@ -114,7 +117,8 @@ def drop_vector_data(username):
 #################
 
 # Define the number of docs to retrieve from the vectorstore and memory
-top_k = 4
+top_k_vectorstore = 4
+top_k_memory = 3
 
 # Define the language options
 lang_options = {
@@ -136,163 +140,167 @@ name, authentication_status, username = authenticator.login('Login', 'sidebar')
 
 if authentication_status:
     authenticator.logout('Logout', 'sidebar')
+
+    # Select the rails experience
+    rails_dict = rails(username)
+
+    #######################
+    ### Resources Cache ###
+    #######################
+
+    # Cache Astra DB session for future runs
+    with st.sidebar:
+        @st.cache_resource(show_spinner=lang_dict['connect_astra'])
+        def load_session():
+            # Connect to Astra DB
+            cluster = Cluster(cloud={'secure_connect_bundle': st.secrets['ASTRA_SCB_PATH']}, 
+                            auth_provider=PlainTextAuthProvider(st.secrets['ASTRA_CLIENT_ID'], 
+                                                                st.secrets['ASTRA_CLIENT_SECRET']))
+            return cluster.connect()
+        session = load_session()
+
+    # Cache Vector Store for future runs
+    with st.sidebar:
+        @st.cache_resource(show_spinner=lang_dict['get_vectorstore'])
+        def load_vectorstore(username):
+            return Cassandra(
+                embedding=OpenAIEmbeddings(openai_api_key=st.secrets['OPENAI_API_KEY']),
+                session=session,
+                keyspace='vector_preview',
+                table_name=f"vector_context_{username}"
+            )
+        vectorstore = load_vectorstore(username)
+
+    ################
+    ### Main app ###
+    ################
+
+    st.title(lang_dict['title'])
+
+    # Include the upload form for new data to be Vectorized
+    with st.sidebar:
+        with st.form('upload'):
+            uploaded_file = st.file_uploader(lang_dict['load_context'], type=['txt', 'pdf'], )
+            submitted = st.form_submit_button(lang_dict['load_context_button'])
+            if submitted:
+                vectorize_text(uploaded_file)
+
+    # Drop the vector data and start from scratch
+    if username == 'michel':
+        with st.sidebar:
+            with st.form('drop'):
+                st.caption(lang_dict['drop_context'])
+                submitted = st.form_submit_button(lang_dict['drop_context_button'])
+                if submitted:
+                    with st.spinner(lang_dict['dropping_context']):
+                        vectorstore.clear()
+                        st.session_state.messages = [AIMessage(content=lang_dict['assistant_welcome'])]
+                        st.session_state.memory.clear()
+
+    # Draw rails
+    with st.sidebar:
+            st.subheader(rails_dict[0])
+            st.caption(rails_dict[1])
+            for i in rails_dict:
+                if i>1:
+                    st.markdown(f"{i-1}. {rails_dict[i]}")
+
+    # Start with empty memory
+    if 'memory' not in st.session_state:
+        st.session_state.memory = ConversationBufferWindowMemory(
+            return_messages=True,
+            k=top_k_memory
+        )
+
+    # Start with empty messages, stored in session state
+    if 'messages' not in st.session_state:
+        st.session_state.messages = [AIMessage(content=lang_dict['assistant_welcome'])]
+
+    # Redraw all messages, both user and agent so far (every time the app reruns)
+    for message in st.session_state.messages:
+        st.chat_message(message.type).markdown(message.content)
+
+    # Now get a prompt from a user
+    if question := st.chat_input(lang_dict['assistant_question']):
+        # Add the prompt to messages, stored in session state
+        st.session_state.messages.append(HumanMessage(content=question))
+
+        # Draw the prompt on the page
+        with st.chat_message('human'):
+            st.markdown(question)
+
+        # Get the results from Langchain
+        with st.chat_message('assistant'):
+            # UI placeholder to start filling with agent response
+            response_placeholder = st.empty()
+
+            history = st.session_state.memory.load_memory_variables({})
+            log.info(f"Getting LLM response for: {question}")
+            log.info(f"Using memory: {history}")
+
+            callback = StreamHandler(response_placeholder)
+
+            # Cache OpenAI Chat Model for future runs
+            model = ChatOpenAI(
+                model='gpt-3.5-turbo-16k',
+                openai_api_key=st.secrets['OPENAI_API_KEY'],
+                streaming=True,
+                verbose=True,
+                callbacks=[callback]
+                )
+
+            retriever = vectorstore.as_retriever(
+                    search_kwargs={'k': top_k_vectorstore}
+                )
+
+            # Cache Conversational Chain for future runs
+            template = """
+            You're a helpful AI assistent tasked to answer the user's questions.
+            You're friendly and you answer extensively with multiple sentences. You prefer to use bulletpoints to summarize.
+            If you don't know the answer, just say 'I do not know the answer'.
+
+            Use the following context to answer the question:
+            {context}
+
+            Use the previous chat history to answer the question:
+            {history}
+
+            Question:
+            {question}
+
+            Answer in the user's language:
+            """
+
+            prompt = ChatPromptTemplate.from_template(template)
+
+            chain = RunnableMap({
+                'context': lambda x: retriever.get_relevant_documents(x['question']),
+                'history': lambda x: x['history'],
+                'question': lambda x: x['question']
+            }) | prompt | model
+
+            response = chain.invoke({'question': question, 'history': history})
+
+            log.info(f"Response: {response}")
+
+            # Write the final answer without the cursor
+            response_placeholder.markdown(response.content)
+
+            # Add the result to memory
+            st.session_state.memory.save_context({'question': question}, {'output': response.content})
+
+            # Add the answer to the messages session state
+            st.session_state.messages.append(AIMessage(content=response.content))
+
 elif authentication_status == False:
     with st.sidebar:
         st.error('Username/password is incorrect')
     st.cache_resource.clear()
     st.session_state.clear()
-    logging.info('Quitting for authentication')
-    st.stop()
+    log.info('Quitting for authentication')
 elif authentication_status == None:
     with st.sidebar:
         st.warning('Please enter your username and password')
     st.cache_resource.clear()
     st.session_state.clear()
-    logging.info('Quitting for authentication')
-    st.stop()
-
-# Select the rails experience
-rails_dict = rails(username)
-
-#######################
-### Resources Cache ###
-#######################
-
-# Cache Astra DB session for future runs
-with st.sidebar:
-    @st.cache_resource(show_spinner=lang_dict['connect_astra'])
-    def load_session():
-        # Connect to Astra DB
-        cluster = Cluster(cloud={'secure_connect_bundle': st.secrets['ASTRA_SCB_PATH']}, 
-                        auth_provider=PlainTextAuthProvider(st.secrets['ASTRA_CLIENT_ID'], 
-                                                            st.secrets['ASTRA_CLIENT_SECRET']))
-        return cluster.connect()
-    session = load_session()
-
-# Cache Vector Store for future runs
-vectorstore = Cassandra(
-    embedding=OpenAIEmbeddings(openai_api_key=st.secrets['OPENAI_API_KEY']),
-    session=session,
-    keyspace='vector_preview',
-    table_name=f"vector_context_{username}"
-)
-
-################
-### Main app ###
-################
-
-st.title(lang_dict['title'])
-
-# Include the upload form for new data to be Vectorized
-with st.sidebar:
-    with st.form('upload'):
-        uploaded_file = st.file_uploader(lang_dict['load_context'], type=['txt', 'pdf'], )
-        submitted = st.form_submit_button(lang_dict['load_context_button'])
-        if submitted:
-            vectorize_text(uploaded_file)
-
-# Drop the vector data and start from scratch
-if username == 'michel':
-    with st.sidebar:
-        with st.form('drop'):
-            st.caption(lang_dict['drop_context'])
-            submitted = st.form_submit_button(lang_dict['drop_context_button'])
-            if submitted:
-                with st.spinner(lang_dict['dropping_context']):
-                    vectorstore.clear()
-                    st.session_state.messages = [AIMessage(content=lang_dict['assistant_welcome'])]
-                    st.session_state.memory.clear()
-
-# Draw rails
-with st.sidebar:
-        st.subheader(rails_dict[0])
-        st.caption(rails_dict[1])
-        for i in rails_dict:
-            if i>1:
-                st.markdown(f"{i-1}. {rails_dict[i]}")
-
-# Start with empty memory
-if 'memory' not in st.session_state:
-    st.session_state.memory = ConversationBufferWindowMemory(
-        return_messages=True,
-        k=top_k
-    )
-
-# Start with empty messages, stored in session state
-if 'messages' not in st.session_state:
-    st.session_state.messages = [AIMessage(content=lang_dict['assistant_welcome'])]
-
-# Redraw all messages, both user and agent so far (every time the app reruns)
-for message in st.session_state.messages:
-    st.chat_message(message.type).markdown(message.content)
-
-# Now get a prompt from a user
-if question := st.chat_input(lang_dict['assistant_question']):
-     # Add the prompt to messages, stored in session state
-    st.session_state.messages.append(HumanMessage(content=question))
-
-    # Draw the prompt on the page
-    with st.chat_message('human'):
-        st.markdown(question)
-
-    # Get the results from Langchain
-    with st.chat_message('assistant'):
-        # UI placeholder to start filling with agent response
-        response_placeholder = st.empty()
-
-        history = st.session_state.memory.load_memory_variables({})
-        #print(f"Getting LLM response for: {question}")
-        #print(f"Using memory: {history}")
-
-        callback = StreamHandler(response_placeholder)
-
-        # Cache OpenAI Chat Model for future runs
-        model = ChatOpenAI(
-            openai_api_key=st.secrets['OPENAI_API_KEY'],
-            streaming=True,
-            verbose=True,
-            callbacks=[callback]
-            )
-
-        retriever = vectorstore.as_retriever(
-                search_kwargs={'k': top_k}
-            )
-
-        # Cache Conversational Chain for future runs
-        template = """
-        You're a helpful AI assistent tasked to answer the user's questions.
-        You're friendly and you answer extensively with multiple sentences. You prefer to use bulletpoints to summarize.
-        If you don't know the answer, just say 'I do not know the answer'.
-
-        Use the following context to answer the question:
-        {context}
-
-        Use the previous chat history to answer the question:
-        {history}
-
-        Question:
-        {question}
-
-        Answer in the user's language:
-        """
-
-        prompt = ChatPromptTemplate.from_template(template)
-
-        chain = RunnableMap({
-            'context': lambda x: retriever.get_relevant_documents(x['question']),
-            'history': lambda x: x['history'],
-            'question': lambda x: x['question']
-        }) | prompt | model
-
-        response = chain.invoke({'question': question, 'history': history})
-
-        #print(f"Response: {response}")
-
-        # Write the final answer without the cursor
-        response_placeholder.markdown(response.content)
-
-        # Add the result to memory
-        st.session_state.memory.save_context({'question': question}, {'output': response.content})
-
-        # Add the answer to the messages session state
-        st.session_state.messages.append(AIMessage(content=response.content))
+    log.info('Quitting for authentication')
